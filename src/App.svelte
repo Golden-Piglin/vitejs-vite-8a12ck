41bb45adba9a74045ea769dfcc9c8a230ed234e0
Golden-Piglin <99201215+Golden-Piglin@users.noreply.github.com>
2022-10-13 12:55:51 -0700

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,53 @@
-<h1>hello i am a person</h1>
+<script>
+let age
+ age = 2
+ function onclick() {
+   age = age + 1
+ }
+ function onClick() {
+   age = age - 1
+ }
+ function onCLick() {
+   age = age + 10
+ }
+ function onCLIck() {
+   age = age - 10
+ }
+  </script>
+  
+<h1>
+hello i am a person
+</h1>
+<h1>
+i have a dog, she is {age}
+
+</h1>
+
+<button on:click={onclick}>Change something on this page!</button>
+<button on:click={onClick}>Change something else on this page!!</button>
+<button on:click={onCLick}>Change a third thing on this page!!!</button>
+<button on:click={onCLIck}>How about a fourth thing!!!!</button>
 
 
 <h2>
 dogs are better then cats 
 
 </h2>
-<p> They just are </p>
+
+
+<p>
+ They just are 
+ 
+ </p>
 
 <style>
 h1 {background: aquamarine
 }
+h2 {background: blue
+}
+p {background: navy
+}
 </style>
+
+
 
